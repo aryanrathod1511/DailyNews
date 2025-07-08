9407774bcb39fde8d029234314876e70a53b638c
Aryan Rathod <aryanrathod1515@gmail.com>
2025-07-08 23:18:55 +0530

    credential : false

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({
   origin: 'https://daily-news-delta.vercel.app',
-  credentials: true
+  credentials: false
 }));
 app.options('*', cors());
 
